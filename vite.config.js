f3e0e0f3ac4840dbb9ca946492c326c01961135a
tobenot <42083410+tobenot@users.noreply.github.com>
2025-02-15 18:43:14 +0800

    bugfix: deploy blank

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+// 注意：确保此文件位于项目根目录
 export default defineConfig({
+  // 设置部署时的基础路径
+  base: '/travel-compass/',
   plugins: [vue()],
 }) 
